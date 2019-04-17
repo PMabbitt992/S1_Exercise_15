2685d8c81704048ea5d1eb74d7eff70000dbb4a0
Paige Mabbitt <PMabbitt992@west-mec.org>
2019-04-17 15:25:41 -0700

    Day 4
    Added custom validation lines for each individual input to make sure they are valid, skeleton outline for luhn algorithm

--- co_payment.js
+++ co_payment.js
@@ -60,4 +60,100 @@ window.onload = function () {
       document.forms.order.elements.subtotal.value = formFields[17];
       document.forms.order.elements.salesTax.value = formFields[19];
       document.forms.order.elements.totalCost.value = formFields[21];
+
 };
+
+window.addEventListener("load", function () {
+      document.getElementById("subButton").onclick = runSubmit;
+      document.getElementById("cardName").oninput = validateName;
+      document.getElementById("getMonth").onchange = validateMonth;
+      document.getElementById("getYear").onchange = validateYear;
+});
+
+function runSubmit() {
+      validateName();
+      validateCredit();
+      validateNumber();
+      validateMonth();
+      validateYear();
+      validateCVC();
+}
+
+function validateCVC() {
+      var cardCVC = document.getElementById("cvc");
+      var creditCard = document.querySelector('input[name="credit"]:checked').value;
+      if (cardCVC.validity.valueMissing) {
+            cardCVC.setCustomValidity("Enter your CVC number");
+      } else if (creditCard === "amex" && (/^\d{4}$/.test(cardCVC.value) === false)) {
+            cardCVC.setCustomValidity("Enter a 4 digit CVC number");
+      } else if (creditCard !== "amex" && (/^\d{3}$/.test(cardCVC.value) === false)) {
+            cardCVC.setCustomValidity("Enter a 3 digit CVC number");
+      } else {
+            cardCVC.setCustomValidity("");
+      }
+}
+
+function validateMonth() {
+      var cardMonth = document.getElementById("expMonth");
+      if (cardMonth.selectedIndex === 0) {
+            cardMonth.setCustomValidity("Select the expiration month");
+      } else {
+            cardMonth.setCustomValidity("");
+      }
+}
+
+function validateYear() {
+      var cardYear = document.getElementById("expYear");
+      if (cardYear.selectedIndex === 0) {
+            cardYear.setCustomValidity("Select the expiration year");
+      } else {
+            cardYear.setCustomValidity("");
+      }
+}
+
+function validateNumber() {
+      var cardNumber = document.getElementById("cardNumber");
+      if (cardNumber.validity.valueMissing) {
+            cardNumber.setCustomValidity("Enter your card number");
+      } else if (cardNumber.validity.patternMismatch) {
+            cardNumber.setCustomValidity("Enter a valid card number");
+      } else {
+            cardNumber.setCustomValidity("");
+
+      }
+}
+
+
+function validateCredit() {
+      var creditCard = document.forms.payment.elements.credit[0];
+      if (creditCard.validity.valueMissing) {
+            creditCard.setCustomValidity("Select credit card");
+      } else {
+            creditCard.setCustomValidity("");
+      }
+}
+
+function validateName() {
+      var cardName = document.getElementById("cardName");
+      if (cardName.validity.valueMissing) {
+            cardName.setCustomValidity("Enter your name as it appears on the card");
+      } else {
+            cardName.setCustomValidity("");
+      }
+}
+
+function sumDigits(numStr) {
+      var digitTotal = 0;
+      for (let i = 0; i < numStr.length; i++) {
+            digitTotal += parseInt(numStr.charAt(i));
+      }
+      return digitTotal;
+}
+
+function luhn(idNum) {
+      //retrieve odd number digits
+
+      //retrieve even digits and double 
+
+      //return whether the sum of digits is divisible by ten
+}
